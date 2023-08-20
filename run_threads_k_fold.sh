f75f89eed10195c5057b27dad67e95b815e3b630
Zhenyu (Allen) Zhang <zhenyu.zhang@utexas.edu>
2023-08-20 11:42:35 -0500

    Update run_threads_k_fold.sh

--- run_threads_k_fold.sh
+++ run_threads_k_fold.sh
@@ -24,81 +24,81 @@ mkdir -p logs/
 mkdir -p results/
 
 
-# ------------- Quantum -----------------
-read -u5
-{
-  method=QUBOBipartiteProjectedCommunityDetection
-  echo $method
-  time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds -o results/WPM > logs/ctcd-WPM.log 2>&1
-  time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds -o results/WPM -r $recommender > logs/ctqr-WPM.log 2>&1
-  echo ''>&5
-}&
-read -u5
-{
-  method=QUBOBipartiteProjectedCommunityDetection
-  echo $method-implicit
-  time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds -o results/WPM-i --implicit > logs/ctcd-WPM-i.log 2>&1
-  time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds -o results/WPM-i --implicit -r $recommender > logs/ctqr-WPM-i.log 2>&1
-  echo ''>&5
-}&
+# # ------------- Quantum -----------------
+# read -u5
+# {
+#   method=QUBOBipartiteProjectedCommunityDetection
+#   echo $method
+#   time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds -o results/WPM > logs/ctcd-WPM.log 2>&1
+#   time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds -o results/WPM -r $recommender > logs/ctqr-WPM.log 2>&1
+#   echo ''>&5
+# }&
+# read -u5
+# {
+#   method=QUBOBipartiteProjectedCommunityDetection
+#   echo $method-implicit
+#   time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds -o results/WPM-i --implicit > logs/ctcd-WPM-i.log 2>&1
+#   time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds -o results/WPM-i --implicit -r $recommender > logs/ctqr-WPM-i.log 2>&1
+#   echo ''>&5
+# }&
 
-# ------------- Our method ----------
-# ------------- Cascade -------------
-beta_list=(0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625)
-# beta_list=(0.5 0.25 0.125 0.0625 0.03125 0.015625)
-method_list=(QUBOBipartiteProjectedCommunityDetection)
-for method in ${method_list[*]}
-do
-  for beta in ${beta_list[*]}
-  do
-  read -u5
-  {
-    tag=Cascade-$method-$beta
-    echo $tag
-    time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds --attribute -b $beta -o results/$tag > logs/ctcd-$tag.log 2>&1
-    time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds --attribute -b $beta -o results/$tag -r $recommender > logs/ctqr-$tag.log 2>&1
-    echo ''>&5
-  }&
-  read -u5
-  {
-    tag=Cascade-$method-$beta-implicit
-    echo $tag
-    time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds --attribute -b $beta -o results/$tag --implicit > logs/ctcd-$tag.log 2>&1
-    time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds --attribute -b $beta -o results/$tag --implicit -r $recommender > logs/ctqr-$tag.log 2>&1
-    echo ''>&5
-  }&
-  done
-done
-# -------------- Quantity ---------------
-method_list=(LTBipartiteProjectedCommunityDetection)
-T_list=(1 3 5)
-# alpha_list=(0.1 0.05 0.01 0.005 0.001 0.0005 0.0001 0)
-alpha_list=(0.1 0.01 0.001 0.0001 0)
-for method in ${method_list[*]}
-do
-  for T in ${T_list[*]}
-  do
-    for alpha in ${alpha_list[*]}
-    do
-    read -u5
-    {
-      tag=Quantity-$method-$T-$alpha
-      echo $tag
-      time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds -a $alpha -t $T -o results/$tag > logs/ctcd-$tag.log 2>&1
-      time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds -a $alpha -t $T -o results/$tag -r $recommender > logs/ctqr-$tag.log 2>&1
-      echo ''>&5
-    }&
-    read -u5
-    {
-      tag=Quantity-$method-$T-$alpha-implicit
-      echo $tag
-      time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds -a $alpha -t $T -o results/$tag --implicit > logs/ctcd-$tag.log 2>&1
-      time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds -a $alpha -t $T -o results/$tag --implicit -r $recommender > logs/ctqr-$tag.log 2>&1
-      echo ''>&5
-    }
-    done
-  done
-done
+# # ------------- Our method ----------
+# # ------------- Cascade -------------
+# beta_list=(0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625)
+# # beta_list=(0.5 0.25 0.125 0.0625 0.03125 0.015625)
+# method_list=(QUBOBipartiteProjectedCommunityDetection)
+# for method in ${method_list[*]}
+# do
+#   for beta in ${beta_list[*]}
+#   do
+#   read -u5
+#   {
+#     tag=Cascade-$method-$beta
+#     echo $tag
+#     time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds --attribute -b $beta -o results/$tag > logs/ctcd-$tag.log 2>&1
+#     time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds --attribute -b $beta -o results/$tag -r $recommender > logs/ctqr-$tag.log 2>&1
+#     echo ''>&5
+#   }&
+#   read -u5
+#   {
+#     tag=Cascade-$method-$beta-implicit
+#     echo $tag
+#     time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds --attribute -b $beta -o results/$tag --implicit > logs/ctcd-$tag.log 2>&1
+#     time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds --attribute -b $beta -o results/$tag --implicit -r $recommender > logs/ctqr-$tag.log 2>&1
+#     echo ''>&5
+#   }&
+#   done
+# done
+# # -------------- Quantity ---------------
+# method_list=(LTBipartiteProjectedCommunityDetection)
+# T_list=(1 3 5)
+# # alpha_list=(0.1 0.05 0.01 0.005 0.001 0.0005 0.0001 0)
+# alpha_list=(0.1 0.01 0.001 0.0001 0)
+# for method in ${method_list[*]}
+# do
+#   for T in ${T_list[*]}
+#   do
+#     for alpha in ${alpha_list[*]}
+#     do
+#     read -u5
+#     {
+#       tag=Quantity-$method-$T-$alpha
+#       echo $tag
+#       time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds -a $alpha -t $T -o results/$tag > logs/ctcd-$tag.log 2>&1
+#       time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds -a $alpha -t $T -o results/$tag -r $recommender > logs/ctqr-$tag.log 2>&1
+#       echo ''>&5
+#     }&
+#     read -u5
+#     {
+#       tag=Quantity-$method-$T-$alpha-implicit
+#       echo $tag
+#       time python kfold_LT_community_detection.py $method -d $dataset -k $n_folds -a $alpha -t $T -o results/$tag --implicit > logs/ctcd-$tag.log 2>&1
+#       time python kfold_LT_qa_recommendation.py   $method -d $dataset -k $n_folds -a $alpha -t $T -o results/$tag --implicit -r $recommender > logs/ctqr-$tag.log 2>&1
+#       echo ''>&5
+#     }
+#     done
+#   done
+# done
 # ------------- Each Item ---------------
 read -u5
 {
